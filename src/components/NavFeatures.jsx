3b7c0d3432b5b63cc24277da43e5b0b0ce792886
joelepore <giuseppejoelepore@gmail.com>
2025-01-13 15:12:57 +0100

    feat: create navfeatures component

--- src/components/NavFeatures.jsx
+++ src/components/NavFeatures.jsx
@@ -1,13 +1,22 @@
+import imgDigitalComics from '../assets/img/buy-comics-digital-comics.png'
+import imgMerchandise from '../assets/img/buy-comics-merchandise.png'
+import imgSubscription from '../assets/img/buy-comics-subscriptions.png'
+import imgShopLocator from '../assets/img/buy-comics-shop-locator.png'
+import imgPowerVisa from '../assets/img/buy-dc-power-visa.svg'
+
 const NavFeatures = () => {
   return (
-    <nav>
-      <ul>
-        <li><a href="#"><img src="" alt="" />Digital Comics</a></li>
-        <li><a href="#"><img src="" alt="" />DC Merchandise</a></li>
-        <li><a href="#"><img src="" alt="" />Subscription</a></li>
-        <li><a href="#"><img src="" alt="" />Comic Shop Locator</a></li>
-        <li><a href="#"><img src="" alt="" />DC Power Visa</a></li>
-      </ul>
+    <nav className="nav-features">
+      <div className="container">
+
+        <ul>
+          <li><a href="#"><img src={imgDigitalComics} alt="" />Digital Comics</a></li>
+          <li><a href="#"><img src={imgMerchandise} alt="" />DC Merchandise</a></li>
+          <li><a href="#"><img src={imgSubscription} alt="" />Subscription</a></li>
+          <li><a href="#"><img src={imgShopLocator} alt="" />Comic Shop Locator</a></li>
+          <li><a href="#"><img src={imgPowerVisa} alt="" />DC Power Visa</a></li>
+        </ul>
+      </div>
     </nav>
   )
 }
