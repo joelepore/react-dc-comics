b2a497b6e764bf5700ac14aa3c18c971ca39e229
joelepore <giuseppejoelepore@gmail.com>
2025-01-13 15:42:21 +0100

    feat: create footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,74 +1,85 @@
 import Button from "./partials/Button"
+import imgFooterLogo from "../assets/img/dc-logo-bg.png"
+import imgFacebook from "../assets/img/footer-facebook.png"
+import imgTwitter from "../assets/img/footer-twitter.png"
+import imgYouTube from "../assets/img/footer-youtube.png"
+import imgPinterest from "../assets/img/footer-pinterest.png"
+import imgPeriscope from "../assets/img/footer-periscope.png"
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer-top">
-        <div className="footer-top-col">
-          <div className="footer-top-list">
-            <h4>DC Comics</h4>
-            <ul>
-              <li><a href="#">Characters</a></li>
-              <li><a href="#">Comics</a></li>
-              <li><a href="#">Movies</a></li>
-              <li><a href="#">TV</a></li>
-              <li><a href="#">Games</a></li>
-              <li><a href="#">Videos</a></li>
-              <li><a href="#">News</a></li>
-            </ul>
+        <div className="container">
+          <div className="footer-top-col">
+            <div className="footer-top-list">
+              <h4>DC Comics</h4>
+              <ul>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
+            </div>
+            <div className="footer-top-list">
+              <h4>Shop</h4>
+              <ul>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+              </ul>
+            </div>
           </div>
-          <div className="footer-top-list">
-            <h4>Shop</h4>
-            <ul>
-              <li><a href="#">Shop DC</a></li>
-              <li><a href="#">Shop DC Collectibles</a></li>
-            </ul>
+          <div className="footer-top-col">
+            <div className="footer-top-list">
+              <h4>DC</h4>
+              <ul>
+                <li><a href="#">Terms of Use</a></li>
+                <li><a href="#">Privacy Policy (New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Substriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className="footer-top-col">
-          <div className="footer-top-list">
-            <h4>DC</h4>
-            <ul>
-              <li><a href="#">Terms of Use</a></li>
-              <li><a href="#">Privacy Policy (New)</a></li>
-              <li><a href="#">Ad Choices</a></li>
-              <li><a href="#">Advertising</a></li>
-              <li><a href="#">Jobs</a></li>
-              <li><a href="#">Substriptions</a></li>
-              <li><a href="#">Talent Workshops</a></li>
-              <li><a href="#">CPSC Certificates</a></li>
-              <li><a href="#">Ratings</a></li>
-              <li><a href="#">Shop Help</a></li>
-              <li><a href="#">Contact Us</a></li>
-            </ul>
+          <div className="footer-top-col">
+            <div className="footer-top-list">
+              <h4>Sites</h4>
+              <ul>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className="footer-top-col">
-          <div className="footer-top-list">
-            <h4>Sites</h4>
-            <ul>
-              <li><a href="#">DC</a></li>
-              <li><a href="#">MAD Magazine</a></li>
-              <li><a href="#">DC Kids</a></li>
-              <li><a href="#">DC Universe</a></li>
-              <li><a href="#">DC Power Visa</a></li>
-            </ul>
-          </div>
+          <img src={imgFooterLogo} alt="Logo" />
         </div>
       </div>
       <div className="footer-bottom">
-        <Button text={"SIGN-UP NOW!"} />
-        <div>
-          <a href="#">Follow Us</a>
-          <nav>
-            <ul>
-              <li><a href="#"><img src="" alt="" /></a></li>
-              <li><a href="#"><img src="" alt="" /></a></li>
-              <li><a href="#"><img src="" alt="" /></a></li>
-              <li><a href="#"><img src="" alt="" /></a></li>
-              <li><a href="#"><img src="" alt="" /></a></li>
-            </ul>
-          </nav>
+        <div className="container">
+          <Button text={"SIGN-UP NOW!"} />
+          <div className="footer-bottom-right">
+            <a href="#">Follow Us</a>
+            <nav>
+              <ul>
+                <li><a href="#"><img src={imgFacebook} alt="Facebook Logo" /></a></li>
+                <li><a href="#"><img src={imgTwitter} alt="Twitter Logo" /></a></li>
+                <li><a href="#"><img src={imgYouTube} alt="YouTube Logo" /></a></li>
+                <li><a href="#"><img src={imgPinterest} alt="Pinterest Logo" /></a></li>
+                <li><a href="#"><img src={imgPeriscope} alt="Periscope Logo" /></a></li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </footer>
--- src/components/partials/Button.jsx
+++ src/components/partials/Button.jsx
@@ -1,7 +1,15 @@
+// Creo un oggetto style che passo all'inline style
+const style = {
+  fontWeight: 'bold',
+  color: 'white',
+  fontSize: '1.1rem',
+  padding: '1rem',
+  border: '3px solid #0282f9'
+}
 // Prendo in input una prop di nome text che viene passata nel footer, in questo modo rendo il mio bottone dinamico
 const Button = ({ text }) => {
   return (
-    <a href="#">{text}</a>
+    <a href="#" style={style}>{text}</a>
   )
 }
 
