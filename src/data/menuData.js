5aa151341f4532bbfe5d69f667b6248e4962d5c9
joelepore <giuseppejoelepore@gmail.com>
2025-01-14 13:56:30 +0100

    data: add data folder with arrays of objects

---
+++ src/data/menuData.js
@@ -0,0 +1,12 @@
+export const navItems = [
+  { id: 1, url: "#", text: "Characters", isActive: false },
+  { id: 2, url: "#", text: "Comics", isActive: true },
+  { id: 3, url: "#", text: "Movies", isActive: false },
+  { id: 4, url: "#", text: "TV", isActive: false },
+  { id: 5, url: "#", text: "Games", isActive: false },
+  { id: 6, url: "#", text: "Collectibles", isActive: false },
+  { id: 7, url: "#", text: "Videos", isActive: false },
+  { id: 8, url: "#", text: "Fans", isActive: false },
+  { id: 9, url: "#", text: "News", isActive: false },
+  { id: 10, url: "#", text: "Shop", isActive: false }
+];
